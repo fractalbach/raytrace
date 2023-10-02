d4784d3b585f04b2957763ddaf481f7b358088a5
fractalbach <fractalbach@gmail.com>
2023-10-02 02:47:18 -0700

    shading with surface normals

--- src/main.cc
+++ src/main.cc
@@ -5,27 +5,42 @@
 #include <fstream>
 #include <iostream>
 
-// SHOW macro prints the variable name and its value
+// SHOW macro prints a variable's name and then its value
 #define SHOW(a) std::clog << #a << ": " << (a) << std::endl;
 
 // given a sphere in 3d space, and a raycast, does this ray hit the sphere?
-bool hit_sphere(const point3 & center, double radius, const ray & r)
+// returns a time t where the ray hits the sphere, but if the there's no hit, then the second pair returns false.
+std::pair<double, bool> hit_sphere(const point3 & center, double radius, const ray & r)
 {
     vec3   oc = r.origin() - center;
     double a  = dot(r.direction(), r.direction());
     double b  = 2.0 * dot(oc, r.direction());
     double c  = dot(oc, oc) - radius * radius;
     double D  = b * b - 4 * a * c; // discriminant
-    return (D >= 0);
+    if (D < 0)
+    {
+        return std::make_pair(-1.0, false); // the ray did not hit the sphere
+    }
+    else
+    {
+        double t = (-b - sqrt(D)) / (2.0 * a); // where the ray first contacts the sphere
+        return std::make_pair(t, true);        // sphere was hit, so return a value and a true
+    }
 }
 
 // ray_color will directly give a color output for a single raycast.
 color ray_color(const ray & r)
 {
-    // Draw a sphere
-    if (hit_sphere(point3(0, 0, -1), 0.5, r))
+    // Check if the ray hit the sphere
+    auto   result = hit_sphere(point3(0, 0, -1), 0.5, r);
+    double t      = result.first;
+    bool   ok     = result.second;
+
+    // If the sphere was hit, draw the normal vector
+    if (ok)
     {
-        return color(1, 0, 0);
+        vec3 n = unit_vector(r.at(t) - vec3(0, 0, -1));      // calculate the normal
+        return 0.5 * color(n.x() + 1, n.y() + 1, n.z() + 1); // make it look fancy
     }
 
     // Gradiant blue sky background
