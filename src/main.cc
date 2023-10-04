4bcfc78f35d9afaa9a25a97f08ee62886fb6e548
fractalbach <fractalbach@gmail.com>
2023-10-03 19:24:41 -0700

    refactor

--- src/main.cc
+++ src/main.cc
@@ -18,7 +18,7 @@ color ray_color(const ray & r, const hittable & world)
     // Check if the ray hit the object
     hit_record rec;
 
-    if (world.hit(r, 0, infinity, rec))
+    if (world.hit(r, interval(0, infinity), rec))
     {
         return 0.5 * (rec.normal + color(1, 1, 1));
     }
--- src/rtweekend.h
+++ src/rtweekend.h
@@ -1,8 +1,12 @@
 #ifndef RTWEEKEND_H
 #define RTWEEKEND_H
 
+#include "constants.h"
+#include "interval.h"
+#include "ray.h"
+#include "vec3.h"
+
 #include <cmath>
-#include <limits>
 #include <memory>
 
 // Usings
@@ -11,11 +15,6 @@ using std::make_shared;
 using std::shared_ptr;
 using std::sqrt;
 
-// Constants
-
-const double infinity = std::numeric_limits<double>::infinity();
-const double pi       = 3.1415926535897932385;
-
 // Utility Functions
 
 inline double degrees_to_radians(double degrees)
@@ -23,9 +22,6 @@ inline double degrees_to_radians(double degrees)
     return degrees * pi / 180.0;
 }
 
-// Common Headers
-
-#include "ray.h"
-#include "vec3.h"
+// Intervals
 
 #endif
