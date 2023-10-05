5d20f3cbf09033d55e1e1789a33338424ce56d3c
fractalbach <fractalbach@gmail.com>
2023-10-05 00:10:19 -0700

    lambertian reflection

--- src/camera.h
+++ src/camera.h
@@ -93,11 +93,11 @@ private:
         if (world.hit(r, interval(0.001, infinity), rec))
         {
             // DIFFUSION
-            vec3 direction = random_on_hemisphere(rec.normal);
+            vec3 direction = rec.normal + random_unit_vector();
 
             // custom neat hack: combines the normal vector coloring with the diffusion
-            // if (depth == max_depth)
-            //     return 0.8 * (rec.normal + color(1, 1, 1)) * ray_color(ray(rec.p, direction), depth - 1, world);
+            if (depth == max_depth)
+                return 0.8 * (rec.normal + color(1, 1, 1)) * ray_color(ray(rec.p, direction), depth - 1, world);
 
             return 0.5 * ray_color(ray(rec.p, direction), depth - 1, world);
 
