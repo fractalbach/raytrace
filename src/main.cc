ec58bb183f8a37376b3548c07bb17452fd6e8ed5
fractalbach <fractalbach@gmail.com>
2023-10-05 23:23:34 -0700

    fuzzy metal, gold and silver

--- src/main.cc
+++ src/main.cc
@@ -77,16 +77,16 @@ int main(int argc, char * argv[])
 
     auto material_ground = std::make_shared<lambertian>(color(0.1, 0.8, 0.2));
     auto material_center = std::make_shared<lambertian>(color(0.7, 0.3, 0.3));
-    auto material_left   = std::make_shared<metal>(color(0.8, 0.8, 0.8));
-    auto material_right  = std::make_shared<metal>(color(0.8, 0.6, 0.2));
+    auto material_left   = std::make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
+    auto material_right  = std::make_shared<metal>(color(0.8, 0.6, 0.2), 0.7);
 
     world.add(std::make_shared<sphere>(point3(0.0, -100.5, -3.0), 100.0, material_ground));
     world.add(std::make_shared<sphere>(point3(0.0, 0.0, -1.5), 0.5, material_center));
     world.add(std::make_shared<sphere>(point3(-1.0, 0.0, -1.5), 0.5, material_left));
     world.add(std::make_shared<sphere>(point3(1.0, 0.0, -1.5), 0.5, material_right));
 
-    cam.samples_per_pixel = fancy ? 128 : 16;
-    cam.max_depth         = fancy ? 64 : 8;
+    cam.samples_per_pixel = fancy ? 128 : 8;
+    cam.max_depth         = fancy ? 32 : 8;
 
     // world.add(std::make_shared<sphere>(point3(0, 0, -1), 0.5));        // front-most
     // world.add(std::make_shared<sphere>(point3(1, 0, -2), 0.5));        // right sidekick
--- src/material.h
+++ src/material.h
@@ -7,7 +7,7 @@
 
 class material
 {
-public:
+  public:
     virtual ~material() = default;
 
     virtual bool scatter(const ray & r_in, const hit_record & rec, color & attenuation, ray & scattered) const = 0;
@@ -15,7 +15,7 @@ public:
 
 class lambertian : public material
 {
-public:
+  public:
     lambertian(const color & a) : albedo(a) {}
 
     bool scatter(const ray & r_in, const hit_record & rec, color & attenuation, ray & scattered) const override
@@ -30,25 +30,26 @@ public:
         return true;
     }
 
-private:
+  private:
     color albedo;
 };
 
 class metal : public material
 {
-public:
-    metal(const color & a) : albedo(a) {}
+  public:
+    metal(const color & a, double f) : albedo(a), fuzz(f < 1 ? f : 1) {}
 
     bool scatter(const ray & r_in, const hit_record & rec, color & attenuation, ray & scattered) const override
     {
         vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-        scattered      = ray(rec.p, reflected);
+        scattered      = ray(rec.p, reflected + fuzz * random_unit_vector());
         attenuation    = albedo;
-        return true;
+        return (dot(scattered.direction(), rec.normal) > 0);
     }
 
-private:
-    color albedo;
+  private:
+    color  albedo;
+    double fuzz;
 };
 
 #endif
